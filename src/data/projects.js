3077d698028afcfc7ba85818308498b2e91fa0e4
tikoot <tiko.otarashvili99@gmail.com>
2023-04-15 08:56:45 +0400

    add prjects

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,40 +1,66 @@
 const projects = [
 	{
 		id: 1,
-		title: 'Google Health Platform',
-		category: 'Full Stack projects',
+		title: 'Fitness Website',
+		category: 'Frontend projects',
 		img: require('@/assets/images/web-project-2.jpg'),
-		url: 'https://google.com'
+		url: 'https://tikoot.github.io/fitness-website/'
 	},
 	{
 		id: 2,
-		title: 'Phoenix Digital Agency',
-		category: 'Full Stack projects',
+		title: 'Youtube Clone',
+		category: 'Frontend projects',
 		img: require('@/assets/images/mobile-project-2.jpg'),
+		url: 'https://tikoot.github.io/Youtube-clone/'
 	},
 	{
 		id: 3,
-		title: 'Project Management UI',
-		category: 'Shopify projects',
+		title: 'Pet Shop',
+		category: 'Frontend projects',
 		img: require('@/assets/images/ui-project-1.jpg'),
+		url: 'https://tikoot.github.io/PetShop/'
 	},
 	{
 		id: 4,
-		title: 'Cloud Storage Platform',
-		category: 'Shopify projects',
+		title: 'Beepb.io',
+		category: 'Frontend projects',
 		img: require('@/assets/images/ui-project-2.jpg'),
+		url:'https://tikoot.github.io/Figma/'
 	},
 	{
 		id: 5,
-		title: 'React Social App',
+		title: 'Domains',
 		category: 'Frontend projects',
 		img: require('@/assets/images/mobile-project-1.jpg'),
+		url: 'https://tikoot.github.io/Domains/'
 	},
 	{
 		id: 6,
-		title: 'Apple Design System',
+		title: 'Seat Reservation',
 		category: 'Frontend projects',
 		img: require('@/assets/images/web-project-1.jpg'),
+		url: 'https://tikoot.github.io/Seat-Reservation-Project/'
+	},
+	{
+		id: 7,
+		title: 'Vacation Destination',
+		category: 'Frontend projects',
+		img: require('@/assets/images/web-project-1.jpg'),
+		url: 'https://tikoot.github.io/Vacaton-Destination/'
+	},
+	{
+		id: 8,
+		title: 'Distance Converter',
+		category: 'Frontend projects',
+		img: require('@/assets/images/web-project-1.jpg'),
+		url: 'https://tikoot.github.io/distance-converter/'
+	},
+	{
+		id: 9,
+		title: 'Guess My Number Game',
+		category: 'Frontend projects',
+		img: require('@/assets/images/web-project-1.jpg'),
+		url: 'https://tikoot.github.io/guess-my-number-game/'
 	},
 ];
 
