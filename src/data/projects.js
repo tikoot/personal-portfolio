c59a41dfc1a2def38c9953e041bb0fa96a25e1d3
tikoot <tiko.otarashvili99@gmail.com>
2023-04-18 16:24:06 +0400

    add new project

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,66 +1,87 @@
 const projects = [
 	{
 		id: 1,
+		title: 'Redberry Knight Cup',
+		category: 'Frontend projects',
+		img: require('@/assets/images/web-project-2.jpg'),
+		url: 'https://fanciful-figolla-634fc0.netlify.app/'
+	},
+	{
+		id: 2,
 		title: 'Fitness Website',
 		category: 'Frontend projects',
 		img: require('@/assets/images/web-project-2.jpg'),
 		url: 'https://tikoot.github.io/fitness-website/'
 	},
 	{
-		id: 2,
-		title: 'Youtube Clone',
-		category: 'Frontend projects',
+		id: 3,
+		title: 'Corona Time App',
+		category: 'Full Stack projects',
 		img: require('@/assets/images/mobile-project-2.jpg'),
-		url: 'https://tikoot.github.io/Youtube-clone/'
+		url: 'http://phpstack-984025-3450489.cloudwaysapps.com/'
 	},
 	{
-		id: 3,
+		id: 4,
+		title: 'Covid Questionnaire',
+		category: 'Full Stack projects',
+		img: require('@/assets/images/mobile-project-2.jpg'),
+		url: 'https://main--chipper-salmiakki-b0eace.netlify.app/'
+	},
+	{
+		id: 5,
 		title: 'Pet Shop',
 		category: 'Frontend projects',
 		img: require('@/assets/images/ui-project-1.jpg'),
 		url: 'https://tikoot.github.io/PetShop/'
 	},
 	{
-		id: 4,
+		id: 6,
 		title: 'Beepb.io',
 		category: 'Frontend projects',
 		img: require('@/assets/images/ui-project-2.jpg'),
 		url:'https://tikoot.github.io/Figma/'
 	},
 	{
-		id: 5,
+		id: 7,
 		title: 'Domains',
 		category: 'Frontend projects',
 		img: require('@/assets/images/mobile-project-1.jpg'),
 		url: 'https://tikoot.github.io/Domains/'
 	},
 	{
-		id: 6,
+		id: 8,
 		title: 'Seat Reservation',
 		category: 'Frontend projects',
 		img: require('@/assets/images/web-project-1.jpg'),
 		url: 'https://tikoot.github.io/Seat-Reservation-Project/'
 	},
 	{
-		id: 7,
+		id: 9,
 		title: 'Vacation Destination',
 		category: 'Frontend projects',
 		img: require('@/assets/images/web-project-1.jpg'),
 		url: 'https://tikoot.github.io/Vacaton-Destination/'
 	},
 	{
-		id: 8,
+		id: 10,
 		title: 'Distance Converter',
 		category: 'Frontend projects',
 		img: require('@/assets/images/web-project-1.jpg'),
 		url: 'https://tikoot.github.io/distance-converter/'
 	},
 	{
-		id: 9,
+		id: 11,
 		title: 'Guess My Number Game',
 		category: 'Frontend projects',
 		img: require('@/assets/images/web-project-1.jpg'),
 		url: 'https://tikoot.github.io/guess-my-number-game/'
+	},
+	{
+		id: 12,
+		title: 'Youtube Clone',
+		category: 'Frontend projects',
+		img: require('@/assets/images/mobile-project-2.jpg'),
+		url: 'https://tikoot.github.io/Youtube-clone/'
 	},
 ];
 
